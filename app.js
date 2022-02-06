c37ee5e5f0ada666ab6d989ce8091201c51d833f
Tom Clegg <tom@tomclegg.ca>
2022-02-06 05:02:22 -0500

    more

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var words = {}, letters = ''
 m.request('words', {responseType: 'text'}).then(resp => {
-    resp = resp.toUpperCase()
     resp.split('\n').map(word => {
-        words[word] = true
+        if (word == word.toLowerCase())
+            words[word] = true
     })
     var j=0
     for (let i=0; i<resp.length; i++) {
         let c = resp.charCodeAt(i)
-        if (c >= 65 && c <= 90) {
+        if (c >= 97 && c <= 122) {
             letters = letters + resp[i]
             j++
         }
@@ -36,6 +36,7 @@ const Letter = {
     view: letter => {
         return m('.', {
             style: {
+                background: letter.attrs.background,
                 display: 'inline-block',
                 padding: '0.1em',
                 border: '1px dashed #555',
@@ -46,62 +47,148 @@ const Letter = {
                 width: '1.1em',
                 height: '1.1em',
                 textAlign: 'center',
+                cursor: 'pointer',
             },
-            onclick: letter.attrs.take,
+            onclick: e => {
+                e.preventDefault()
+                letter.attrs.take()
+            },
         }, [
             letter.attrs.rune || '-',
         ])
     },
 }
-
-const Board = {
-    oninit: board => {
-        const gridw = 8, gridh = 8
-        board.state.rows = []
-        for (let y=0; y<gridh; y++) {
-            let r = []
-            for (let x=0; x<gridw; x++) {
-                r.push(null)
-            }
-            board.state.rows.push(r)
-        }
-        board.state.next = 'A'
-    },
-    view: board => {
-        if (!words.RIGHTEOUS) return
-        return m('.', {
-            style: {
-                background: '#000',
-                width: '100%',
-                height: '100%',
-            },
-        }, [
-            m(Letter, {rune: board.state.next}),
-            board.state.rows.map((cols, row) => {
-                return [
-                    m('br'),
-                    cols.map((rune, col) => {
-                        return m(Letter, {
-                            rune: rune,
-                            take: _ => {
-                                if (rune) return
-                                board.state.rows[row][col] = board.state.next
-                                board.state.next = letters[Math.floor(Math.random()*letters.length)]
-                            },
-                        })
-                    }),
-                ]
-            }),
-            m('br'),
-            m(Button, {
-                onclick: e => {
-                    board.state.rows.map(cols => {
-                        return cols.map((_, col) => { cols[col] = null })
-                    })
+function Score() {
+    return {
+        view: vnode => {
+            return m('.', {
+                style: {
+                    background: vnode.attrs.background,
+                    display: 'inline-block',
+                    padding: '0.1em',
+                    border: '1px dashed #555',
+                    borderRadius: '0.1em',
+                    color: vnode.attrs.color,
+                    fontSize: '3em',
+                    fontWeight: 'bold',
+                    width: '4em',
+                    height: '1.1em',
+                    textAlign: 'right',
                 },
-            }, 'clear'),
-        ])
-    },
+            }, [
+                vnode.attrs.score,
+            ])
+        },
+    }
+}
+
+function Board() {
+    const gridw = 8, gridh = 8, minwordlen = 3
+    var rows, next, prizes, highlight, score, hiscore
+    return {
+        oninit: vnode => {
+            board = []
+            for (let y=0; y<gridh; y++) {
+                let r = []
+                for (let x=0; x<gridw; x++) {
+                    r.push(null)
+                }
+                board.push(r)
+            }
+            next = 'a'
+            prizes = []
+            highlight = {}
+            score = 0
+            hiscore = window.localStorage.getItem('hiscore') || 0
+        },
+        view: vnode => {
+            if (!words.righteous) return
+            return m('.', {
+                style: {
+                    background: '#000',
+                    width: '100%',
+                    height: '100%',
+                },
+            }, [
+                m(Letter, {rune: next, background: '#442'}),
+                m(Score, {score: score, background: '#030', color: '#bfb'}),
+                m(Score, {score: hiscore, background: '#030', color: '#9b9'}),
+                board.map((cols, row) => {
+                    return [
+                        m('br'),
+                        cols.map((rune, col) => {
+                            return m(Letter, {
+                                rune: rune,
+                                background: highlight[''+row+','+col] ? '#050' : '#000',
+                                take: _ => {
+                                    if (rune) return
+                                    board[row][col] = next
+                                    let prev = next
+                                    while (prev == next)
+                                        next = letters[Math.floor(Math.random()*letters.length)]
+                                    givePrizes(row, col)
+                                },
+                            })
+                        }),
+                    ]
+                }),
+                m('br'),
+                m(Button, {
+                    onclick: e => {
+                        e.preventDefault()
+                        board.map(cols => {
+                            return cols.map((_, col) => { cols[col] = null })
+                        })
+                        prizes = []
+                        next = 'a'
+                        highlight = {}
+                        score = 0
+                    },
+                }, 'clear'),
+                m('br'),
+                m('p', {
+                    style: {
+                        color: '#bfb',
+                    },
+                }, prizes.map(word => {
+                    return [word.toLowerCase(), ' ']
+                })),
+            ])
+        },
+    }
+    function givePrizes(prizeRow, prizeCol) {
+        highlight = {}
+        for (let startcol=0; startcol<=prizeCol; startcol++)
+            for (var endcol=Math.max(prizeCol+1, startcol+minwordlen); endcol<=gridw; endcol++) {
+                var word = ''
+                for (let col=startcol; col<endcol; col++)
+                    word += board[prizeRow][col] || ' '
+                if (words[word]) {
+                    prizes.push(word)
+                    score += Math.floor(Math.exp(word.length))
+                    for (let col=startcol; col<endcol; col++)
+                        highlight[''+prizeRow+','+col] = true
+                } else
+                    console.log('no "'+word+'"')
+            }
+        for (let startrow=0; startrow<=prizeRow; startrow++)
+            for (let endrow=Math.max(prizeRow+1, startrow+minwordlen); endrow<=gridh; endrow++) {
+                var word = ''
+                for (let row=startrow; row<endrow; row++)
+                    word += board[row][prizeCol] || ' '
+                if (words[word]) {
+                    prizes.push(word)
+                    score += Math.floor(Math.exp(word.length))
+                    for (let row=startrow; row<endrow; row++)
+                        highlight[''+row+','+prizeCol] = true
+                } else
+                    console.log('no '+word)
+            }
+        if (score > hiscore) {
+            hiscore = score
+            window.localStorage.setItem('hiscore', hiscore)
+        }
+    }
 }
 
 document.body.style.margin = 0
